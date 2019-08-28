78893032f57da7d43954cdf560d751c1ff7886bd
Armugan Ashraf <>
2019-08-27 23:58:40 -0400

    added data of the meal plan

--- client/src/components/MealPlan/Cuisine.js
+++ client/src/components/MealPlan/Cuisine.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
- 
+import { Week, Cuisines } from '../../Data/cuisine';
 class Cuisine extends React.Component{
+  state = {
+    cuisine : []
+  }
   render(){
+    
     return(
       <>
       </>
